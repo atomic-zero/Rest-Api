de0e653458ed402e04bbe7e881c68f9720a29d48
atomic-zero <lkpanio25@gmail.com>
2024-09-20 02:31:31 +0800

    test

--- api/ai.js
+++ api/ai.js
@@ -9,7 +9,7 @@ exports.config = {
     version: '1.0.0',
     author: 'Kenneth Panio',
     description: 'Interact with GPT-4O API.',
-    usage: ['/ai?prompt=hello'],
+    usage: ['/gpt4om?prompt=hello'],
     category: 'ai',
 };
 
