07a6315dea0f377e63ffb6fb7b36618e9d9994b6
atomic-zero <lkpanio25@gmail.com>
2024-09-20 01:45:05 +0800

    trabaho

--- api/ai.js
+++ api/ai.js
@@ -1,7 +1,5 @@
 const axios = require('axios');
 const randomUseragent = require('random-useragent');
-const fs = require('fs');
-const path = require('path');
 
 const conversationHistories = {};
 
