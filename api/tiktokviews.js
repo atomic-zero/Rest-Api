052df1b963283598a09718fe1004bea4a619301e
atomic-zero <lkpanio25@gmail.com>
2024-10-31 22:16:09 +0800

    haha

--- api/tiktokviews.js
+++ api/tiktokviews.js
@@ -1,6 +1,18 @@
 
 const axios = require('axios');
-const randomUseragent = require('random-useragent');
+
+// Define a list of iOS user agents
+const iosUserAgents = [
+  "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1",
+  "Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1",
+  "Mozilla/5.0 (iPod touch; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1"
+  // Add more iOS user agents as needed
+];
+
+// Function to get a random iOS user agent
+const getRandomIOSUserAgent = () => {
+  return iosUserAgents[Math.floor(Math.random() * iosUserAgents.length)];
+};
 
 exports.config = {
   name: "tiktokviews",
@@ -23,8 +35,8 @@ const createHeaders = () => {
     "accept": "application/json, text/plain, */*",
     "content-type": "application/json",
     "sec-ch-ua-mobile": "?1",
-    "user-agent": randomUseragent.getRandom(),
-    "sec-ch-ua-platform": "\"Android\"",
+    "user-agent": getRandomIOSUserAgent(), // Use the iOS user agent
+    "sec-ch-ua-platform": "\"iOS\"", // Change platform to iOS
     "origin": "https://likesjet.com",
     "sec-fetch-site": "same-site",
     "sec-fetch-mode": "cors",
@@ -37,14 +49,12 @@ const createHeaders = () => {
 const boostViews = async (username, link) => {
   const email = generateRandomEmail();
   const headers = createHeaders();
-
   try {
     const response = await axios.post('https://api.likesjet.com/freeboost/3', {
       link: link,
       tiktok_username: username,
       email: email
     }, { headers: headers });
-
     return { success: true, data: response.data };
   } catch (error) {
     const statusCode = error.response ? error.response.status : 500;
@@ -55,20 +65,12 @@ const boostViews = async (username, link) => {
 
 exports.initialize = async ({ req, res }) => {
   const { username, link } = req.query;
-
   if (!username || !link) {
     return res.status(400).json({ error: 'Please provide both username and link.' });
   }
-
   const result = await boostViews(username, link);
-
   if (result.success) {
-    res.json({
-      status: true,
-      message: `Successfully boosted views for ${username}.`,
-      data: result.data,
-      author: exports.config.author
-    });
+    res.json({ status: true, message: `Successfully boosted views for ${username}.`, data: result.data, author: exports.config.author });
   } else {
     res.status(result.statusCode).json({ error: `An error occurred while boosting views: ${result.error}` });
   }
