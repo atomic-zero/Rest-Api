269af63f145033a7f7e82964aa1a67fac874b20b
atomic-zero <lkpanio25@gmail.com>
2025-01-18 11:16:52 +0800

    jsjsjsjs

--- api/screenshot.js
+++ api/screenshot.js
@@ -2,9 +2,6 @@ const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
 
-// Valid URL regex to ensure it starts with http:// or https://
-const urlRegex = /^(https?:\/\/)([a-z0-9-]+\.)+[a-z]{2,6}\/?$/i;
-
 exports.config = {
     name: "screenshot",
     category: "tools",
@@ -14,22 +11,92 @@ exports.config = {
     author: "Kenneth Panio",
 };
 
-// Fetch and stream the screenshot
+const getHeadersForUrl = (url) => {
+    const domainPatterns = [{
+        domains: ['pixiv.net', 'i.pximg.net'],
+        headers: {
+            Referer: 'http://www.pixiv.net/'
+        }
+    },
+    {
+        domains: ['deviantart.com'],
+        headers: {
+            Referer: 'https://www.deviantart.com/'
+        }
+    },
+    {
+        domains: ['artstation.com'],
+        headers: {
+            Referer: 'https://www.artstation.com/'
+        }
+    },
+    {
+        domains: ['instagram.com'],
+        headers: {
+            Referer: 'https://www.instagram.com/'
+        }
+    },
+    {
+        domains: ['googleusercontent.com'],
+        headers: {
+            Referer: 'https://images.google.com/'
+        }
+    },
+    {
+        domains: ['i.nhentai.net', 'nhentai.net'],
+        headers: {
+            Referer: 'https://nhentai.net/'
+        }
+    }];
+
+    const domain = domainPatterns.find(({
+        domains
+    }) => domains.some(d => url.includes(d)));
+
+    const headers = domain ? {
+        ...domain.headers
+    } : {};
+
+    if (url.endsWith('.jpg') || url.endsWith('.png')) {
+        headers['Accept'] = 'image/webp,image/apng,image/*,*/*;q=0.8';
+    }
+
+    return headers;
+};
+
+// Download the image and return it as arraybuffer
+const download = async (url, responseType = 'arraybuffer', extension = 'png') => {
+    try {
+        const response = await axios.get(url, {
+            responseType,
+            headers: getHeadersForUrl(url),
+        });
+
+        if (responseType === 'arraybuffer') {
+            // Send the image as a stream (no need to save locally)
+            return response.data;
+        }
+
+        return response.data;
+    } catch (error) {
+        console.error("Download error:", error.message);
+        throw new Error("Failed to download the image.");
+    }
+};
+
 const fetchAndStreamScreenshot = async (url, res) => {
     try {
         const thumUrl = `https://image.thum.io/get/width/1920/crop/400/fullpage/noanimate/${encodeURIComponent(url)}`;
-        const response = await axios({
-            url: thumUrl,
-            method: "GET",
-            responseType: "stream",
-        });
+        
+        // Fetch image as arraybuffer (streamable data)
+        const imageBuffer = await download(thumUrl, "arraybuffer");
 
         // Set headers to stream the image as a response
         res.setHeader("Content-Type", "image/png");
         res.setHeader("Content-Disposition", "inline; filename=screenshot.png");
 
-        // Pipe the image stream to the response
-        response.data.pipe(res);
+        // Stream the image buffer directly to the response
+        res.end(imageBuffer);
 
         return true;
     } catch (error) {
@@ -49,7 +116,8 @@ exports.initialize = async ({ req, res }) => {
         });
     }
 
-    // Validate URL format using regex
+    // Validate URL format using regex (only http/https)
+    const urlRegex = /^(https?:\/\/)([a-z0-9-]+\.)+[a-z]{2,6}\/?$/i;
     if (!urlRegex.test(url)) {
         return res.status(400).json({
             error: "Invalid URL. Only URLs starting with 'http://' or 'https://' are allowed.",
